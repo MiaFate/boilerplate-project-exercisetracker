fb77fc47f7c38b517feaed6543f324dbd5dbf7db
miafate <miapurplefroot@gmail.com>
2024-01-17 22:02:10 -0300

    added endpoints

--- index.js
+++ index.js
@@ -1,13 +1,124 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+//bd configuration
+mongoose.connect(process.env.MONGO_URI)
+
+//basic configuration
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+const UserModel = require("./models/users.js");
+const ExerciseModel = require("./models/exercises.js");
+
+//this endpoint is for fetch a users list as array
+app.get('/api/users', async function(req, res) {
+  try {
+    const list = await UserModel.find({})
+    const filteredList = list.map(user => {
+      return { username: user.username, _id: user._id }
+    })
+    res.status(200).json(filteredList)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+//this endpoint is for create new users
+app.post('/api/users', async function(req, res) {
+  try {
+    const { username } = req.body
+    if (req.body.username === "") throw new Error("you must provide a username")
+
+    const userAlreadyExists = await UserModel.findOne({ username });
+    if (userAlreadyExists) {
+      throw new Error("user already exists on the database")
+    }
+
+    const newUser = new UserModel({ username });
+    const doc = await newUser.save()
+
+    res.status(200).json({ username: doc.username, _id: doc._id })
+  } catch (error) {
+    res.json({ error: error.message })
+  }
+})
+
+app.post('/api/users/:_id/exercises', async function(req, res) {
+  try {
+    const { _id } = req.params
+    let { description, duration, date } = req.body;
+    const user = await UserModel.findById(_id)
+    if (!user) throw new Error("User not found")
+
+    pastDate = new Date(date)
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let dayName = pastDate.getUTCDay()
+    let day = pastDate.getUTCDate()
+    let month = pastDate.getUTCMonth()
+    let year = pastDate.getUTCFullYear()
+    fixedDate = date ? `${days[dayName]} ${months[month]} ${day} ${year}` : new Date().toDateString()
+
+
+    const newExercise = new ExerciseModel({
+      userId: _id,
+      description,
+      duration: parseInt(duration),
+      date: fixedDate
+    })
+    const exercise = await newExercise.save()
+
+    res.status(200).json({
+      username: user.username,
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date,
+      _id: exercise.userId
+    })
+  } catch (error) {
+    res.json({ error: error.message })
+  }
+
+})
+
+app.get('/api/users/:_id/logs', async function(req, res) {
+  try {
+    const { _id } = req.params
+
+    const user = await UserModel.findById(_id)
+    if (!user) throw new Error("User not found")
+
+    const list = await ExerciseModel.find({})
+
+    const log = list.map(({ description, duration, date }) => {
+      return {
+        description,
+        duration,
+        date
+      }
+    })
+
+    const logs = {
+      username: user.username,
+      count: log.length,
+      _id: user._id,
+      log
+    }
+
+    res.json(logs)
+  } catch (error) {
+    res.json({ error: error.message })
+  }
+})
 
 
 
